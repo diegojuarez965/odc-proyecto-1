b620b6cd8c0831502adc83278317c41e34c24f65
unknown <diegohernanjuarez965@gmail.com>
2021-10-07 15:43:06 -0300

    commit 6

--- leerInput.c
+++ leerInput.c
@@ -166,6 +166,103 @@ int* inputValido(char *argv[], int argc)
     }
 }
 
+//Mapea un entero expresado con digitos a un entero expresado con caracteres.
+void mapearDigitos(char *numero,int *numeroEntero)
+{
+    int *i;
+    i= malloc(sizeof(int));
+    *i=0;
+    while(numeroEntero[*i]!=-1)
+    {
+        switch(numeroEntero[*i])
+        {
+        case 0:
+        {
+            numero[*i]='0';
+            break;
+        }
+        case 1:
+        {
+            numero[*i]='1';
+            break;
+        }
+        case 2:
+        {
+            numero[*i]='2';
+            break;
+        }
+        case 3:
+        {
+            numero[*i]='3';
+            break;
+        }
+        case 4:
+        {
+            numero[*i]='4';
+            break;
+        }
+        case 5:
+        {
+            numero[*i]='5';
+            break;
+        }
+        case 6:
+        {
+            numero[*i]='6';
+            break;
+        }
+        case 7:
+        {
+            numero[*i]='7';
+            break;
+        }
+        case 8:
+        {
+            numero[*i]='8';
+            break;
+        }
+        case 9:
+        {
+            numero[*i]='9';
+            break;
+        }
+        case 10:
+        {
+            numero[*i]='A';
+            break;
+        }
+        case 11:
+        {
+            numero[*i]='B';
+            break;
+        }
+        case 12:
+        {
+            numero[*i]='C';
+            break;
+        }
+        case 13:
+        {
+            numero[*i]='D';
+            break;
+        }
+        case 14:
+        {
+            numero[*i]='E';
+            break;
+        }
+        case 15:
+        {
+            numero[*i]='F';
+            break;
+        }
+        }
+        *i=*i+1;
+    }
+    numero[*i]='\0';
+    free(i);
+}
+
 //Mapea un entero expresado con caracteres a un entero expresado con dígitos finalizado en -1.
 void mapearLetras(char *numero,int *numeroEntero)
 {
@@ -269,7 +366,15 @@ void mapearLetras(char *numero,int *numeroEntero)
     free(i);
 }
 
-void mostrarAyuda();
+void mostrarAyuda()
+{
+    printf("Parámetros de invocación:\n");
+    printf("-n <numero>: admite solo positivos, con la parte entera hasta 10 digitos y la fraccionaria hasta 5 digitos\n");
+    printf("-s <base origen> : admite 2 a 16\n");
+    printf("-d <base destino> : admite 2 a 16\n");
+    printf("-v : muestra computos intermedios\n");
+    printf("-h : muestra este mensaje\n");
+}
 
 //Se fija si el caracter indicado por un entero en un arreglo de caracteres es valido segun los posibles caracteres que se puedan recibir segun el formato del enunciado.
 int* caracterValido(char* palabra, int* indice)
