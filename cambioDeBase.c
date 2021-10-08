882fb83cce113b9fbc05072bad77b30ccee22845
unknown <diegohernanjuarez965@gmail.com>
2021-10-08 17:12:48 -0300

    commit 10

--- cambioDeBase.c
+++ cambioDeBase.c
@@ -1,5 +1,12 @@
 #include "cambioDeBase.h"
 
+/**
+ * Transforma la parte entera de un numero en cierta base a base 10, mostrando o no los computos intermedios.
+ * @param origen Base origen.
+ * @param n  Parte entera de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte entera de un numero en base 10.
+*/
 int *enteroXa10 (int *origen, int *n, int *mostrar)
 {
     int *ultDig;
@@ -73,6 +80,13 @@ int *enteroXa10 (int *origen, int *n, int *mostrar)
     return toReturn;
 }
 
+/**
+ * Transforma la parte fraccionaria de un numero en cierta base a base 10, mostrando o no los computos intermedios.
+ * @param origen Base origen.
+ * @param n  Parte fraccionaria de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte fraccionaria de un numero en base 10.
+*/
 int *fraccionXa10 (int * origen, int * n, int * mostrar)
 {
     double *suma;
@@ -136,6 +150,13 @@ int *fraccionXa10 (int * origen, int * n, int * mostrar)
     return toReturn;
 }
 
+/**
+ * Transforma la parte entera de un numero en base 10 a cierta base, mostrando o no los computos intermedios.
+ * @param destino Base destino.
+ * @param n  Parte entera de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte entera de un numero en base destino.
+*/
 int * entero10aX(int *destino, int *n, int *mostrar)
 {
     int *resultadoTentativo;
@@ -206,6 +227,13 @@ int * entero10aX(int *destino, int *n, int *mostrar)
     return resultadoFinal;
 }
 
+/**
+ * Transforma la parte fraccionaria de un numero en base 10 a cierta base, mostrando o no los computos intermedios.
+ * @param destino Base destino.
+ * @param n  Parte fraccionaria de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte fraccionaria de un numero en base destino.
+*/
 int *fraccion10aX(int *destino, int *n, int *mostrar)
 {
     int *resultado;
@@ -257,11 +285,27 @@ int *fraccion10aX(int *destino, int *n, int *mostrar)
     return resultado;
 }
 
+/**
+ * Transforma la parte entera de un numero en cierta a base a cierta base, mostrando o no los computos intermedios.
+ * @param origen Base origen.
+ * @param destino Base destino.
+ * @param n  Parte entera de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte entera de un numero en base destino.
+*/
 int *enteroXaY(int *origen, int *destino, int *n, int *mostrar)
 {
     return entero10aX(destino,enteroXa10(origen,n,mostrar),mostrar);
 }
 
+/**
+ * Transforma la parte fraccionaria de un numero en cierta a base a cierta base, mostrando o no los computos intermedios.
+ * @param origen Base origen.
+ * @param destino Base destino.
+ * @param n  Parte fraccionaria de un numero a cambiar de base.
+ * @param mostrar Si es 0 no muestra los computos intermedios, si es 1 muestra los computos intermedios.
+ * @return Parte fraccionaria de un numero en base destino.
+*/
 int *fraccionXaY(int *origen, int *destino, int *n, int *mostrar)
 {
     return fraccion10aX(destino,fraccionXa10(origen,n,mostrar),mostrar);
